17ee01d69515d9ff9ecb97b3c3fc6faf2f09b87d
kentrpg <kent00rpg@gmail.com>
2025-07-08 11:01:22 +0800

    refactor(picker): simplify path handling and code structure

--- scripts/picker.ts
+++ scripts/picker.ts
@@ -5,6 +5,8 @@ import { execa } from 'execa'
 import prompts from 'prompts'
 
 async function startPicker(args: string[]) {
+  const subdirectory = 'src'
+  const slidevName = 'slides.md'
   const folders = (await fs.readdir(new URL('..', import.meta.url), { withFileTypes: true }))
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
@@ -25,33 +27,34 @@ async function startPicker(args: string[]) {
   args = args.filter(arg => arg !== '-y')
 
   if (result.folder) {
+    const srcPath = new URL(`../${result.folder}/${subdirectory}`, import.meta.url)
+    const slidevPath = new URL(`${srcPath}/${slidevName}`, import.meta.url)
+
     if (args[0] === 'dev')
-      execa('cursor', [fileURLToPath(new URL(`../${result.folder}/src/slides.md`, import.meta.url))])
+      execa('cursor', [fileURLToPath(slidevPath)])
     if (args[0] === 'build') {
       await execa('pnpm', ['rimraf', 'dist'], {
-        cwd: new URL(`../${result.folder}/src`, import.meta.url),
+        cwd: srcPath,
         stdio: 'inherit',
       })
     }
     if (args[0] === 'export') {
-      const slidesPath = new URL(`../${result.folder}/src/slides.md`, import.meta.url)
-      const slidesContent = await fs.readFile(slidesPath, 'utf-8')
-      const frontmatterMatch = slidesContent.match(/^---([\s\S]*?)---/)
-      let title = ''
       let fileName = result.folder
-      if (frontmatterMatch) {
-        const frontmatter = frontmatterMatch[1]
-        const titleMatch = frontmatter.match(/title:\s*(.+)/)
-        if (titleMatch) {
-          title = titleMatch[1].trim().replace(/^['"]|['"]$/g, '')
-        }
+
+      const content = await fs.readFile(slidevPath, 'utf-8')
+      const frontmatter = content.match(/^---([\s\S]*?)---/)
+      let [, title] = frontmatter?.[1]?.match(/title:\s*(.+)/) || []
+      if (title) {
+        title = title.trim().replace(/^['"]|['"]$/g, '')
+        const slugTitle = title.replace(/[^\w\-\u4E00-\u9FA5 ]+/g, '').replace(/\s+/g, '-')
+        if (slugTitle)
+          fileName = slugTitle
       }
-      if (title)
-        fileName = title.replace(/[^\w\-\u4E00-\u9FA5 ]+/g, '').replace(/\s+/g, '-')
+
       args.push('--output', `../${fileName}.pdf`)
     }
     await execa('pnpm', ['run', ...args], {
-      cwd: new URL(`../${result.folder}/src`, import.meta.url),
+      cwd: srcPath,
       stdio: 'inherit',
     })
   }
